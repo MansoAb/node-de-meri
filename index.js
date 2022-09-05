9451387e426f3b93804b2ecb7952acc3d8cbb022
Абубакаров Мансур Хайжахметович <abubakarovmansurhajzahmetovic@MacBook-Air-Abubakarov.local>
2022-09-05 17:19:23 +0300

    did everything

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import os from "os"
-
+import {type} from './welcome.js'
 console.log(os.homedir())
 console.log(os.hostname())
 console.log(os.version())
 console.log(os.type())
+type()
---
+++ welcome.js
@@ -0,0 +1,6 @@
+import os from "os"
+
+export function type(){
+    console.log("Привет, юзер. Вижу ты зашел с " + os.type())
+}
+
